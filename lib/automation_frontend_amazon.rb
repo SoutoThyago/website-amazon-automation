7a94b151eab0cc8757a4d330b97802167be526a6
Thyago <thyago@teste.com.br>
2023-03-18 18:31:30 -0300

    first commit

---
+++ lib/automation_frontend_amazon.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "automation_frontend_amazon/version"
+
+module AutomationFrontendAmazon
+  class Error < StandardError; end
+  # Your code goes here...
+end
